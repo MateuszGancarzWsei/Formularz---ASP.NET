adc04f932061fc58c37699192447b0d2674c11a1
MateuszGancarzWsei <93799998+MateuszGancarzWsei@users.noreply.github.com>
2021-11-06 00:28:26 +0100

    Add files via upload

---
+++ FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMvc1.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Nazwa elementu")]
+        public string Name { get; set; }
+        [Display(Name = "Data urodzenia")]
+        public string Data { get; set; }
+        [Display(Name = "Ilość dni od urodzenia")]
+        public string Days { get; set; }
+    }
+}
